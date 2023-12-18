697fd8bde15374bdadd18bcbececf4f594b5bdf2
Ryan-Felix <130946315+Ryan-Felix@users.noreply.github.com>
2023-12-18 17:12:48 -0300

    Update index.js
    Acréscimo do Banco de Dados e otimização

--- src/screens/Paciente/Delete/index.js
+++ src/screens/Paciente/Delete/index.js
@@ -1,50 +1,48 @@
-import { FlatList, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, Alert } from 'react-native';
 import styles from './style';
-import Input from '../../../components/InputBar';
 import { useRoute } from '@react-navigation/native'
 
-export default function HomeP({ navigation }) {
-
+export default function DeletePaciente({navigation}) {
   const routes = useRoute();
-
   const GetP = () => {
-    navigation.navigate('GetP');
+  navigation.navigate('GetP');
   };
 
-  const PostP = () => {
-    navigation.navigate('PostP');
-  };
+  const [numeroBeneficiario, setNumeroBeneficiario] = useState('');
 
-  const PutP = () => {
-    navigation.navigate('PutP');
-  };
+  const handleDelete = async () => {
+    try {
+      const response = await fetch(`http://etec199-2021-robsondias.atwebpages.com/SW/clinicaPHP/delete/delete.php?numeroBeneficiario=${numeroBeneficiario}`, {
+        method: 'DELETE',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
 
-  const DeleteP = () => {
-    navigation.navigate('DeleteP');
+      const data = await response.json();
+
+      if (response.ok) {
+        GetP();
+      } else {
+        Alert.alert('Erro', data.mensagem);
+      }
+    } catch (error) {
+      console.error('Erro ao excluir paciente:', error);
+    }
   };
 
   return (
     <View style={styles.container}>
-      <View style={styles.row}>
-        <View style={styles.column}>
-          <View style={styles.inputDiv}>
-            <Text style={styles.Text}>Nº Beneficiário:</Text>
-            <Input style={styles.input} />
-          </View>
-        </View>
-      </View>
-
-
-      <TouchableOpacity style={styles.sendButton} onPress={''}>
-          <Text style={styles.sendButtonText}>Enviar</Text>
-        </TouchableOpacity>
-
-
+      <Text style={styles.text}>Excluir Paciente</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Número Beneficiário"
+        onChangeText={(text) => setNumeroBeneficiario(text)}
+        value={numeroBeneficiario}
+        keyboardType="numeric"
+      />
+      <Button title="Excluir Paciente" onPress={handleDelete} />
     </View>
-
-    
-
-
-
   );
-};
+}
