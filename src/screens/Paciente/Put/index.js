f489410e17bf4b9f4a74e2679b38efc33cd24804
Ryan-Felix <130946315+Ryan-Felix@users.noreply.github.com>
2023-12-18 17:32:13 -0300

    Update index.js

--- src/screens/Paciente/Put/index.js
+++ src/screens/Paciente/Put/index.js
@@ -1,101 +1,82 @@
-import React from 'react';
-import { useRoute } from '@react-navigation/native';
-import Input from '../../../components/InputBar';
-import { Text, View, Image, TouchableOpacity } from 'react-native';
-import { FontAwesome } from '@expo/vector-icons';
+import React, { useState } from 'react';
 import styles from './style';
+import { View, Text, TextInput, Button } from 'react-native';
+import { useRoute } from '@react-navigation/native';
 
-
-export default function Telas({ navigation }) {
-
+const PutP = ({ navigation }) => {
+  const routes = useRoute();
   const GetP = () => {
     navigation.navigate('GetP');
   };
+  const [numeroBeneficiario, setNumeroBeneficiario] = useState('');
+  const [nome, setNome] = useState('');
+  const [telefone, setTelefone] = useState('');
+  const [endereco, setEndereco] = useState('');
+  const [remedio, setRemedio] = useState('');
+  const [doencas, setDoencas] = useState('');
 
-  const PostP = () => {
-    navigation.navigate('PostP');
+  const handleUpdate = async () => {
+    try {
+      const response = await fetch('http://etec199-2021-robsondias.atwebpages.com/SW/clinicaPHP/update/update.php', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          numeroBeneficiario,
+          nome,
+          telefone,
+          endereco,
+          remedio,
+          doencas,
+        }),
+      });
+
+      console.log('Status da resposta:', response.status);
+
+      const data = await response.json();
+      console.log('Resposta do servidor:', data);
+
+      if (data.erro) {
+        console.error('Erro ao atualizar paciente:', data.erro);
+      } else {
+        console.log('Paciente atualizado com sucesso. ID Inserido:', data.idInserido);
+        setNome('');
+        setTelefone('');
+        setEndereco('');
+        setRemedio('');
+        setDoencas('');
+
+        GetP();
+      }
+    } catch (error) {
+      console.error('Erro ao atualizar dados do servidor:', error);
+    }
   };
 
-  const DeleteP = () => {
-    navigation.navigate('DeleteP');
-  };
-
-
-  const routes = useRoute();
   return (
     <View style={styles.container}>
+      <Text style={styles.text}>Número do Beneficiário:</Text>
+      <TextInput style={styles.input} value={numeroBeneficiario} onChangeText={setNumeroBeneficiario} />
 
-      <Image
-        source={require('../../../Img/PacientePut.png')}
-        style={styles.imagem}
-      />
+      <Text style={styles.text}>Nome:</Text>
+      <TextInput style={styles.input} value={nome} onChangeText={setNome} />
 
-      <View style={styles.inputDiv}>
-        <Text style={styles.text}>Nº Beneficiário:</Text>
-        <Input style={styles.input} />
-      </View>
-      <View style={styles.inputDiv}>
-        <Text style={styles.text}>Nome:</Text>
-        <Input style={styles.input} />
-      </View>
-      <View style={styles.inputDiv}>
-        <Text style={styles.text}>Telefone:</Text>
-        <Input style={styles.input} />
-      </View>
+      <Text style={styles.text}>Telefone:</Text>
+      <TextInput style={styles.input} value={telefone} onChangeText={setTelefone} />
 
+      <Text style={styles.text}>Endereço:</Text>
+      <TextInput style={styles.input} value={endereco} onChangeText={setEndereco} />
 
-      <View style={styles.inputDiv}>
-        <Text style={styles.text}>Endereço:</Text>
-        <Input style={styles.input} />
-      </View>
-      <View style={styles.inputDiv}>
-        <Text style={styles.text}>Remédio de Uso Contínuo:</Text>
-        <Input style={styles.input} />
-      </View>
-      <View style={styles.inputDiv}>
-        <Text style={styles.text}>Doenças Prévias:</Text>
-        <Input style={styles.input} />
-      </View>
+      <Text style={styles.text}>Remédio:</Text>
+      <TextInput style={styles.input} value={remedio} onChangeText={setRemedio} />
 
+      <Text style={styles.text}>Doenças:</Text>
+      <TextInput style={styles.input} value={doencas} onChangeText={setDoencas} />
 
-      <TouchableOpacity style={styles.sendButton} onPress={''}>
-          <Text style={styles.sendButtonText}>Enviar</Text>
-        </TouchableOpacity>
-
-
-
-      <View style={styles.row}>
-        <TouchableOpacity style={styles.square} onPress={GetP}>
-          <View>
-            <FontAwesome name="arrow-circle-up" size={30} color="white" />
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.square} onPress={PostP}>
-          <View>
-            <FontAwesome name="plus" size={30} color="white" />
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={[styles.square, styles.ActButton]} >
-          <View>
-            <FontAwesome name="pencil" size={30} color="white" />
-          </View>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.square} onPress={DeleteP}>
-          <View>
-            <FontAwesome name="trash" size={30} color="white" />
-          </View>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.square} >
-          <View>
-            <FontAwesome name="undo" size={30} color="white" />
-          </View>
-        </TouchableOpacity>
-
-        
-
-
-      </View>
+      <Button title="Atualizar" onPress={handleUpdate} style={{borderRadius: 100}} />
     </View>
   );
-}
+};
+
+export default PutP;
